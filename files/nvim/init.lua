5f0ac4ae82741f5327328473ef7b1b9972b47868
Sebastian Barszczewski <sebastian.barszczewski@gmail.com>
2023-02-02 09:08:27 +0100

    fixing nvim treesitter plugin initialization, further nvim tweaks

--- files/nvim/init.lua
+++ files/nvim/init.lua
@@ -9,28 +9,61 @@ if not vim.loop.fs_stat(lazypath) then
     lazypath,
   })
 end
+
 vim.opt.rtp:prepend(lazypath)
 
-vim.o.number = true
-vim.o.tabstop = 2
-vim.o.softtabstop = 2
-vim.o.shiftwidth = 2
-vim.o.relativenumber = true
-vim.o.expandtab = true
-vim.o.smartindent = true
-vim.o.nowrap = true
-vim.o.smartcase = true
-vim.o.incsearch = true
+vim.o.number = true -- Print line numbers
+vim.o.tabstop = 2  -- Number of spaces tabs count for
+vim.o.shiftwidth = 2 -- Size of an indent
+vim.o.relativenumber = true -- Relative line numbers
+vim.o.expandtab = true -- Use spaces instead of tabs
+vim.o.smartindent = true -- Automatically insert indents
+vim.o.tabstops = 2 -- Number of spaces tabs count for
+vim.o.wrap = false -- Disable line wrap
+vim.o.smartcase = true -- Don't ignore case with capitals
+vim.o.cursorline = true -- Enable highlighting of current line
+-- vim.o.laststatus = 0
+vim.o.scrolloff = 4 -- Lines of context above and below current line
+vim.o.splitbelow = true -- Put new windows below current
+vim.o.splitright = true -- Put new windows right of current
+
 
 require("lazy").setup({
+
   "unblevable/quick-scope",
+ 
+  {
+    "rmehri01/onenord.nvim",
+    config = function()
+      require("onenord").setup()
+    end
+  },
+
   {
     "nvim-treesitter/nvim-treesitter", 
-    cmd = "TSUpdate",
+    build = ":TSUpdate",
+    event = "BufReadPost",
     config = function()
       require("nvim-treesitter.configs").setup({
           highlight = {
-            ensure_installed = {"lua", "rust", "html", "vim", "help", "java", "javascript", "tsx", "typescript", "yaml", "toml"},
+            ensure_installed = {
+              "lua",
+              "bash",
+              "markdown",
+              "markdown_inline",
+              "regex",
+              "rust", 
+              "go",
+              "html", 
+              "vim", 
+              "help", 
+              "java", 
+              "javascript", 
+              "tsx", 
+              "typescript", 
+              "yaml", 
+              "toml"
+            },
             enable = true,
             use_languagetree = true,
             disable = { "org" },
